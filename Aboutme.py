8f91b44774bd9cf84f3c65b8a4574d27674a52df
unknown <crosebros@gmail.com>
2018-07-24 15:28:26 -0400

    myfirstcommit

---
+++ Aboutme.py
@@ -0,0 +1,6 @@
+Bio:
+	Hi, my name is Carolyn Brosnan.
+   I am a girl who codes from Stamford, CT.
+    I am so excited to finally become a senior at Valhalla High School this year.
+    I look forward to planning out my future and applying to colleges.
+     I live at home with my three sisters, my mom, my Dad and my dog named Bella. 
